0689ade1a26ea136cb76612663599d346328ec15
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-08 23:33:48 +0600

    successfully connect mongodb database using mongoose

--- app.js
+++ app.js
@@ -13,5 +13,12 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.all("/*splat", (req, res) => {
+  res.status(404).json({
+    status: "fail",
+    message: `can't find ${req.originalUrl} on this server`,
+  });
+});
+
 // export the app from file
 module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 require("@dotenvx/dotenvx").config();
 const server = require("./app");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
+
+const DB = process.env.DB_URL.replace("<PASSWORD>", process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("DATABASE CONNECTED!");
+  })
+  .catch((err) => {
+    console.log("DATABASE CONNECTIO FAILED ERROR!", err);
+  });
 
 server.listen(PORT, () => {
   console.log(`server listen on ${PORT}`);
