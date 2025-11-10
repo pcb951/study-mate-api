a9f362017a91863322b560d6386b34db11ba2f3b
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-10 13:29:04 +0600

    handel production level error

--- controllers/globalErrorController.js
+++ controllers/globalErrorController.js
@@ -7,8 +7,26 @@ const sendDevError = (err, res) => {
   });
 };
 
+const sendProductionError = (err, res) => {
+  // Don't send accidentally other error
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // when it was not operational error send the default error
+    res.status(500).json({
+      status: "error",
+      message: "something went very wrong!",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendDevError(err, res);
   }
+
+  sendProductionError(err, res);
 };
