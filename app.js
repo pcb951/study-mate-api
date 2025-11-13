174eef36952f2b6b34f94ec34c093905b684a689
Polas Chandra Barmon <dpi.polas@gmail.com>
2025-11-13 15:34:47 +0600

    update cors middleware

--- app.js
+++ app.js
@@ -14,7 +14,14 @@ const app = express();
 app.use(cookieParser());
 // use json form data parsing middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: [("Content-Type", "Authorization")],
+    credentials: true,
+  })
+);
 
 // requests are logging console
 if (process.env.NODE_ENV === "development") {
